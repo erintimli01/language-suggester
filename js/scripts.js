21dadf2ae2d33be16adac0474c82941c84f89514
Erin Timlin <erintimlin@gmail.com>
2023-01-13 16:45:25 -0500

    add languages and class to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 function displayLanguage() {
-  const ranger = document.getElementsByName('ranger');
-  const dogs = document.getElementsByName('dogs');
-  const football = document.getElementsByName('football');
-  const woods = document.getElementsByName('woods');
-  const spaghetti = document.getElementsByName('spaghetti');
+  const ranger = document.getElementsByName('ranger').setAttribute("class", "hidden");
+  const dogs = document.getElementsByName('dogs').setAttribute("class", "hidden");
+  const football = document.getElementsByName('football').setAttribute("class", "hidden");
+  const woods = document.getElementsByName('woods').setAttribute("class", "hidden");
+  const spaghetti = document.getElementsByName('spaghetti').setAttribute("class", "hidden");
+}
+
+window.onclick = function() {
+
+document.querySelector("form").onclick = function(event) {
+  event.preventDefault();
 
   if (ranger.checked && dogs.checked && football.checked && woods.checked && spaghetti.checked);
-    result 
+    onclick="Javascript" 
 }
+  };
