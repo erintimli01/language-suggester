5203862f3e9b54c1e5f346aed5051fd849b4e57e
Erin Timlin <erintimlin@gmail.com>
2023-01-13 16:28:08 -0500

    add functions to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,10 @@
-function hideResultsAndError(){
-  document.getElementsByName("ranger");
-  document.getElementsByName("dogs");
-  document.getElementsByName("football");
-  document.getElementsByName("woods");
-  document.getElementsByName("spaghetti")
-}
-window.onload = funtion () {
-  hideResultsAndError();
-    
-document.querySelector("form").onsubmit = function (event) {
-  event.preventDefault();
-  hideResultsAndError();
-  
-}
+function displayLanguage() {
+  const ranger = document.getElementsByName('ranger');
+  const dogs = document.getElementsByName('dogs');
+  const football = document.getElementsByName('football');
+  const woods = document.getElementsByName('woods');
+  const spaghetti = document.getElementsByName('spaghetti');
+
+  if (ranger.checked && dogs.checked && football.checked && woods.checked && spaghetti.checked);
+    result 
 }
